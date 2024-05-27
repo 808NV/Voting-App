6683ce38687df72f6c3b014a7e1afa36822035a7
omar <yourname@example.com>
2024-05-27 20:37:52 +0300

    add headline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-
+import { Icons } from "@/components/Icons";
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-50 grid">
       <MaxWidthWrapper className="relative pb-20 pt-10 sm:pb-32 lg:pt-24 xl:pt-32 lg:pb-52">
-        <div>Hello</div>
+        <div className="px-6 lg:px-0 lg-pt-4">
+          <div className="relative mx-auto text-center flex flex-col items-center">
+            <h1 className="relative leading-snug w-fit tracking-tight text-balance mt-16 font-bold text-gray-900 text-5xl md:text-6xl">
+              What do you{" "}
+              <span className="whitespace-nowrap">
+                th
+                <span className="relative">
+                  i
+                  <span className="absolute inset-x-0 -top-2 -translate-x-3">
+                    <Icons.brain className="h-7 w-7 md:h-8 md:w-8" />
+                  </span>
+                </span>
+                nk
+              </span>{" "}
+              about...
+            </h1>
+          </div>
+        </div>
       </MaxWidthWrapper>
     </div>
   );
