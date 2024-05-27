ba2b1e5ff865e9c7c0b492307962c75d0e7d6531
omar <yourname@example.com>
2024-05-27 20:16:51 +0300

    add reusable maxwidthwrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="min-h-screen bg-zinc-50 grid">
+      <MaxWidthWrapper className="relative pb-20 pt-10 sm:pb-32 lg:pt-24 xl:pt-32 lg:pb-52">
+        <div>Hello</div>
+      </MaxWidthWrapper>
+    </div>
+  );
 }
